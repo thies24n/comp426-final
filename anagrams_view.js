6395a717e2c53f41e74da4940bb6b0989238aac5
8d2 <34866771+8d2@users.noreply.github.com>
2024-05-06 18:10:15 -0400

    Update view

--- anagrams_view.js
+++ anagrams_view.js
@@ -9,9 +9,26 @@ export class AnagramsView {
     }
 
     render(render_div) {
+        /*
+            Header
+        */
         let header = document.createElement('h1');
-        header.append("Loading...");
+        header.append("Anagrams Trainer");
         render_div.append(header);
+
+        let mainui = document.createElement('div');
+        mainui.id = "main-ui";
+        mainui.append("");
+        render_div.append(mainui);
+
+        let tile = document.createElement('div');
+        tile.classList.add("tile");
+        tile.append("A");
+        mainui.append(tile);
+        let tile2 = document.createElement('div');
+        tile2.classList.add("tile");
+        tile2.append("B");
+        mainui.append(tile2);
     }
 
 }
